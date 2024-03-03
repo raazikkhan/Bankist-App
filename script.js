9347bc9556a9ee2a8c52016df30d459717906757
Md Razik Khan <mdrazikkhan779@gmail.com>
2024-03-04 01:40:02 +0530

    Add data in script file.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 `use strict`
+//Data for my bankist app
+const account1 = {
+    owner: "Razik Khan",
+    movements: [200,432,-444,349,560,-30,-999,10,-323,-30],
+    intrestRate: 1.7,
+    pin: 0220
+}
+const account2 = {
+    owner: "Amaira Khan",
+    movements: [200,432,-444,4323,-403,232,-543,-903,203,-430],
+    intrestRate: 0.9,
+    pin: 1111
+}
+const account3 = {
+    owner: "Ahad Khan",
+    movements: [200,432,-444,843,-432,-454,300,102,-432,500],
+    intrestRate: 1,
+    pin: 2222
+}
+const account4 = {
+    owner: "Arsheen Khan",
+    movements: [200,432,-444,3044,-321,666,-432,],
+    intrestRate: 1.9,
+    pin: 3333
+}
+const account5 = {
+    owner: "Yusuf Khan",
+    movements: [200,432,-444,],
+    intrestRate: 1.7,
+    pin: 4444
+}
+
+const account6 = {
+    owner: "Ahmad Khan",
+    movements: [800,492,-441,4332,-454,40,322,-30,-800,-29],
+    intrestRate: 2.4,
+    pin: 5555
+}
+const accounts = [account1,account2,account3,account4,account5,account6]
